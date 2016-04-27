5ce5f980a572ea137a4f0e813459ef7a18262eae
L8rlTinkerer <nickumcb@gmail.com>
2016-04-27 21:09:59 +1200

    Changes upto 2.b.iii. Currently having difficulties with confirming the url registration.

---
+++ Gallery3Selfhost/Service1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost
+{
+    class Service1:IService1
+    {
+
+        public List<String> GetArtistNames()
+        {
+            using(Gallery_DataEntities lcContent = new Gallery_DataEntities())
+            {
+                return lcContent.Artists.Select(lcArtist => lcArtist.Name).ToList();
+            }
+        }
+
+    }
+}
